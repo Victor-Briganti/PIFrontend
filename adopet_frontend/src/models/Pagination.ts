8049975706829a751d9fdb0e0406b1e0e2f35366
Victor Briganti <jvbriganti@gmail.com>
2024-05-23 20:44:30 -0300

    [Validate][Adoption] Adicionado validador para adoção

--- adopet_frontend/src/models/Pagination.ts
+++ adopet_frontend/src/models/Pagination.ts
@@ -1,36 +1,6 @@
-export interface PaginationData<T> {
+export default interface PaginationData<T> {
   count: number;
   next: string | null;
   previous: string | null;
   results: T[];
-}
-
-export default class Pagination<T> {
-  private count: number;
-  private next: string | null;
-  private previous: string | null;
-  private results: T[];
-
-  constructor(data: PaginationData<T>) {
-    this.count = data.count;
-    this.next = data.next;
-    this.previous = data.previous;
-    this.results = data.results;
-  }
-
-  getCount(): number {
-    return this.count;
-  }
-
-  getNext(): string | null {
-    return this.next;
-  }
-
-  getPrevious(): string | null {
-    return this.previous;
-  }
-
-  getResults(): T[] {
-    return this.results;
-  }
 }
---
+++ adopet_frontend/src/models/validators/ValidateAdoption.ts
@@ -0,0 +1,50 @@
+import InterfaceAdoption from "../Adoption";
+import { MapStatusChoice } from "../map_choices/MapChoices";
+
+const statusMap = new MapStatusChoice();
+
+export function validatedRegisterAdoption(
+  adoption: InterfaceAdoption
+): InterfaceAdoption {
+  if (adoption.donor < 0) {
+    throw new Error("Doador não pode ter o id negativo");
+  }
+
+  if (adoption.adopter < 0) {
+    throw new Error("Adodante não pode ter o id negativo");
+  }
+
+  if (adoption.animal < 0) {
+    throw new Error("Animal não pode ter o id negativo");
+  }
+
+  adoption.request_status = statusMap.getKeyByValue(
+    adoption.request_status ?? "Pendente"
+  );
+
+  return adoption;
+}
+
+export function validatedUpdateAdoption(
+  adoption: InterfaceAdoption
+): InterfaceAdoption {
+  if (adoption.donor < 0) {
+    throw new Error("Doador não pode ter o id negativo");
+  }
+
+  if (adoption.adopter < 0) {
+    throw new Error("Adodante não pode ter o id negativo");
+  }
+
+  if (adoption.animal < 0) {
+    throw new Error("Animal não pode ter o id negativo");
+  }
+
+  if (adoption.request_status === undefined) {
+    throw new Error("Status de adoção não pode ser indefinido");
+  }
+
+  adoption.request_status = statusMap.getValueByKey(adoption.request_status);
+
+  return adoption;
+}
