9375058fcdce7e5bff2611e249930b9abcd40e80
Victor Briganti <jvbriganti@gmail.com>
2024-06-08 20:20:45 -0300

    [Refatoração] Melhorado tela para erro de navegação

--- adopet_frontend/src/pages/NotFound.tsx
+++ adopet_frontend/src/pages/NotFound.tsx
@@ -1,5 +1,6 @@
-import ErrorLayout from "../components/layouts/ErrorLayout";
 import * as MUI from "@mui/material";
+import * as Router from "react-router-dom";
+import ErrorLayout from "../components/layouts/ErrorLayout";
 
 export default function NotFound() {
   return (
@@ -9,10 +10,28 @@ export default function NotFound() {
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
+          flexDirection: "column",
           height: "100vh",
         }}
       >
         <h1>404 NÃO ENCONTRADO</h1>
+        <MUI.Box sx={{ mt: 2 }}>
+          <MUI.Link
+            component={Router.Link}
+            to={"/"}
+            variant="body2"
+            fontSize={20}
+            sx={{
+              color: "white",
+              textDecoration: "none",
+              "&:hover": {
+                textDecoration: "underline",
+              },
+            }}
+          >
+            Voltar para home
+          </MUI.Link>
+        </MUI.Box>
       </MUI.Box>
     </ErrorLayout>
   );
